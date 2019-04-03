da7a90027211d2958579171dea44294b4d1d1c87
Fedjmike <sam.nipps@gmail.com>
2019-04-04 00:33:13 +0100

    Update schema tests to check query results

--- backend/notes/tests.py
+++ backend/notes/tests.py
@@ -21,11 +21,14 @@ class SchemaTest(TestCase):
     def run_query(self, query, **kwargs):
         result = schema.execute(query, variables=kwargs)
         
-        if result.errors: print(result.errors)
-        self.assertEqual(result.errors, None)
+        if result.errors:
+            if len(result.errors) == 1:
+                raise result.errors[0]
+            
+            else:
+                raise result.errors
         
-        import json
-        print(json.dumps(result.to_dict()))
+        return result.data
     
     def test_notes_schema(self):
         create_some_data()
@@ -33,7 +36,7 @@ class SchemaTest(TestCase):
         query {
             allNotes {
                 created,
-                revisions {
+                latestRevision {
                     text
                 },
                 tags {
@@ -42,7 +45,8 @@ class SchemaTest(TestCase):
             }
         }
         """
-        self.run_query(query)
+        data = self.run_query(query)
+        self.assertEqual(data["allNotes"][0]["latestRevision"]["text"], "n0r0")
         
     def test_tags_schema(self):
         create_some_data()
@@ -59,7 +63,8 @@ class SchemaTest(TestCase):
             }
         }
         """
-        self.run_query(query)
+        data = self.run_query(query)
+        self.assertEqual(data["allTags"][0]["notes"][0]["revisions"][0]["text"], "n0r0")
         
     def test_revision_mutation(self):
         n0 = create_some_data()
@@ -78,7 +83,9 @@ class SchemaTest(TestCase):
             }
         }
         """
-        self.run_query(query, noteId=n0.id)
+        data = self.run_query(query, noteId=n0.id)
+        self.assertEqual(data["createRevision"]["revision"]["text"], "n0r1")
+        self.assertEqual(data["createRevision"]["revision"]["note"]["latestRevision"]["text"], "n0r1")
 
     def test_note_mutation(self):
         query = """
@@ -92,4 +99,6 @@ class SchemaTest(TestCase):
             }
         }
         """
-        self.run_query(query)
+        data = self.run_query(query)
+        self.assertEqual(len(data["createNote"]["note"]["revisions"]), 1)
+        self.assertEqual(data["createNote"]["note"]["revisions"][0]["text"], "")
